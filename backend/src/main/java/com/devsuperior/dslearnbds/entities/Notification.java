a0a0449bbd48e07d6a12f55ad674e4841c43fd41
uevitondev <uevitonsantos@outlook.com>
2023-06-22 15:07:37 -0300

    notification

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -0,0 +1,100 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_notification")
+public class Notification implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private boolean read;
+    private String route;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    public Notification() {
+
+    }
+
+    public Notification(Long id, String text, Instant moment, boolean read, String route, User user) {
+        this.id = id;
+        this.text = text;
+        this.moment = moment;
+        this.read = read;
+        this.route = route;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Notification that = (Notification) o;
+        return read == that.read && Objects.equals(id, that.id) && Objects.equals(text, that.text) && Objects.equals(moment, that.moment) && Objects.equals(route, that.route) && Objects.equals(user, that.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text, moment, read, route, user);
+    }
+}
