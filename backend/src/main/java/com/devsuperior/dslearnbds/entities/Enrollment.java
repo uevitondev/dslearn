a395965c64bc02d32c946aff6f9e6e3dac4b670d
uevitondev <uevitonsantos@outlook.com>
2023-06-22 14:43:12 -0300

    lessons seed, fix: lessonsDone

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -2,13 +2,12 @@ package com.devsuperior.dslearnbds.entities;
 
 import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -22,6 +21,9 @@ public class Enrollment implements Serializable {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonssDone = new HashSet<>();
 
     public Enrollment() {
 
